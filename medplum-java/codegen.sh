cb04874dfcd035c9dca420a37b5cb5f08078465d
Deep Singhvi <dsinghvi@umich.edu>
2022-04-26 19:20:34 -0700

    improvement: add readme and cleanup package names (#4)
    * update sample app subproject name
    
    * add readme

--- medplum-java/codegen.sh
+++ medplum-java/codegen.sh
@@ -1,8 +1,7 @@
-version="b4a5eee"
+version="0.0.1"
 # download plugin
 rm -rf fern-client-cli-"$version"-"$version"
-wget --user "$JFROG_USERNAME" --password "$JFROG_API_KEY" \
-  https://usebirch.jfrog.io/artifactory/default-maven-local/com/fern/java/fern-client-cli/"$version"/fern-client-cli-"$version".zip
+wget https://s01.oss.sonatype.org/content/repositories/releases/io/github/fern-api/fern-client-cli/"$version"/fern-client-cli-"$version".zip
 unzip fern-client-cli-"$version".zip
 cd fern-client-cli-"$version"-"$version"
 # delete existing code
